39460b07baa035d6aade1daa9bbfb3897dbf93af
william19970624 <19970624gsq@gmail.com>
2016-02-12 20:55:05 +0100

    primer

---
+++ _2.1/_2.1.ino
@@ -0,0 +1,12 @@
+void setup()                // se inicia una vez al empezar
+{
+  Serial.begin(9600);       // configurar velocidad a 9600 bps
+  Serial.println(" I wish... "); // escribira debajo
+  Serial println(" ...your dead");// escribira debajo
+}
+
+void loop()   // run over and over again
+{
+       // do nothing
+}
+
