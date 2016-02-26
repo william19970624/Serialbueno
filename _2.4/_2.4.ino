c1584426b7b6bdc12fe9d86e880507654ad3d9a8
william19970624 <19970624gsq@gmail.com>
2016-02-26 20:26:12 +0100

    _2.4

---
+++ _2.4/_2.4.ino
@@ -0,0 +1,12 @@
+void setup() 
+{
+   Serial.begin(9600);                                                     //velocitat de transmission de dades
+   Serial.println("Escull el numero de l'operacio que vols realitzar?");   //linies d'escrit
+   Serial.println("  1.Comprovar numero de tarjeta de credit");            //linies d'escrit  
+   Serial.println("  2.Comprovar numero de compte bancari");               //linies d'escrit
+   Serial.println("  3.Buscar un digit perdut de tarjeta de credit");      //linies d'escrit
+}
+
+void loop() {
+  
+}
