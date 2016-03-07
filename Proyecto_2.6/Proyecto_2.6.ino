6569aeaeaea480f086b47f79942a5881156759fc
william19970624 <19970624gsq@gmail.com>
2016-03-07 22:34:42 +0100

    Proyecto_2.6

---
+++ Proyecto_2.6/Proyecto_2.6.ino
@@ -0,0 +1,41 @@
+/***************************************************************
+**               Projecte 2.                                  **   
+**          Hacer que el arduino scribira palabras            **
+**                                                            **
+***************************************************************/
+
+
+//****** Includes **********************************************
+
+
+//****** Variables *********************************************
+
+
+//****** Setup *************************************************
+
+int drive_gb = 100;
+long drive_mb;            //canviamos de "int" a "long" para que coja mas bytes
+
+
+void setup()              // se inicia una vez al empezar
+{
+  Serial.begin(9600);     // configuracion de la velocidad de datos
+   Serial.print("Your HD is ");
+  Serial.print(drive_gb);
+  Serial.println(" GB large.");
+
+  drive_mb = drive_gb * 1024 ;
+
+  Serial.print("It can store ");
+  Serial.print(drive_mb);
+  Serial.println(" Megabytes!");
+  
+}
+
+
+
+//****** Loop **************************************************
+
+void loop()  // aqui se escribe para que se vaya repitiendo en ciclo
+{
+}
