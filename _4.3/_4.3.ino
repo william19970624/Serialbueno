79a644387e203ea4b5249b6eeb2dd5754d0f57a7
william19970624 <19970624gsq@gmail.com>
2016-03-11 20:12:41 +0100

    4.3

---
+++ _4.3/_4.3.ino
@@ -0,0 +1,46 @@
+/***************************************************************
+**                  Projecte 4.                               **   
+**          Control estructura                                **
+**                                                            **
+***************************************************************/
+
+
+//****** Includes **********************************************
+
+
+//****** Variables *********************************************
+
+
+//****** Setup *************************************************
+
+int tempAigua = 98;                       // configurar el numero 101 com a "tempAigua"
+
+void setup() 
+{
+  Serial.begin(9600);                     // set up Serial library at 9600 bps
+  if( tempAigua < 90)                     // informara de la temperatura de l'aigua 
+  {
+    Serial.print("Aigua encara no bull"); // informara de la temperatura de l'aigua
+  }
+  if ( tempAigua == 100)
+  {
+    Serial.print("Aigua a 100C");         // informara de la temperatura de l'aigua
+  }
+
+  if (tempAigua > 100)
+  {
+    Serial.print("Aigua bullint");        // informara de la temperatura de l'aigua
+  }
+  else 
+  {
+    Serial.print("Aigua apunt de bullir");// informara de la temperatura de l'aigua
+  }
+}
+
+
+
+//****** Loop **************************************************
+
+void loop()                         // el que s'escriu aqui es va repetint en cicle 
+{ 
+} 
