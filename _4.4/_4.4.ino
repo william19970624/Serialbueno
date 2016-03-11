f75af0e0b5a645a79195cfc3f0c42c3e3e31eeca
william19970624 <19970624gsq@gmail.com>
2016-03-11 21:11:32 +0100

    4.4

--- _4.4/_4.4.ino
+++ _4.4/_4.4.ino
@@ -1,5 +1,5 @@
 /***************************************************************
-**                  Projecte 4.                               **   
+**                  Projecte 4.                               **
 **          Control estructura                                **
 **                                                            **
 ***************************************************************/
@@ -13,50 +13,50 @@
 
 //****** Setup *************************************************
 
-int qualificacio = 3.5;                       // configurar el numero 101 com a "tempAigua"
+int qualificacio = 3.5;              // configurar el numero 101 com a "tempAigua"
 
-void setup() 
+void setup()
 {
   Serial.begin(9600);               // set up Serial library at 9600 bps
-    
-  if( qualificacio < 3.5);           // informara de la temperatura de l'aigua 
-  
+
+  if ( qualificacio < 3.5)          // informara de la temperatura de l'aigua
+  {
     Serial.print("A");          // informara de la temperatura de l'aigua
-  
-  else if ( qualificacio < 6.5);
-  
+  }
+  else if ( qualificacio < 6.5)
+  {
     Serial.print("B");         // informara de la temperatura de l'aigua
-  
+  }
 
-  else if (qualificacio <11.1);
-  
+  else if (qualificacio < 11.1)
+  {
     Serial.print("C");        // informara de la temperatura de l'aigua
-  
- 
-  else if (qualificacio <17.7);
+  }
 
+  else if (qualificacio < 17.7)
+  {
     Serial.print("D");        // informara de la temperatura de l'aigua
-  
-  
-  else if (qualificacio <38.2); 
-  
+  }
+
+  else if (qualificacio < 38.2)
+  {
     Serial.print("E");        // informara de la temperatura de l'aigua
-  
-  
-  else if (qualificacio <43.2);
-  
+  }
+
+  else if (qualificacio < 43.2)
+  {
     Serial.print("F");        // informara de la temperatura de l'aigua
-  
-  else  
-  
+  }
+  else
+  {
     Serial.print("G");        // informara de la temperatura de l'aigua
-  
+  }
 }
 
 
 
 //****** Loop **************************************************
 
-void loop()                         // el que s'escriu aqui es va repetint en cicle 
-{ 
-} 
+void loop()                         // el que s'escriu aqui es va repetint en cicle
+{
+}
