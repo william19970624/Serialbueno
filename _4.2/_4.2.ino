ab6ea0d4a5c69ae4e5d89289260ada6ef679f196
william19970624 <19970624gsq@gmail.com>
2016-03-11 19:38:20 +0100

    4.2

---
+++ _4.2/_4.2.ino
@@ -0,0 +1,39 @@
+/***************************************************************
+**                  Projecte 3.                               **   
+**          Hacer que el arduino escribira palabras           **
+**                                                            **
+***************************************************************/
+
+
+//****** Includes **********************************************
+
+
+//****** Variables *********************************************
+
+
+//****** Setup *************************************************
+
+int tempAigua = 101;                 // configurar el numero 101 com a "tempAigua"
+
+void setup() 
+{
+  Serial.begin(9600);                // set up Serial library at 9600 bps
+
+  if ( tempAigua >= 100)              // informara de la temperatura de l'aigua 
+  {
+    Serial.print("Aigua bullint");
+  } 
+
+  else 
+  {
+    Serial.print("Aigua encara no bull");
+  }
+}
+
+
+
+//****** Loop **************************************************
+
+void loop()                         // el que s'escriu aqui es va repetint en cicle 
+{ 
+} 
